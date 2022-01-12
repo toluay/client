7b0be5def7433cb5659d39eb1ba003f2d8eae288
toluay <ayantundetolu@gmail.com>
2022-01-12 10:35:28 +0000

    backend url added the the base uri in the api

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/' });
+const API = axios.create({ baseURL: 'https://mernappbackend1292.herokuapp.com/' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
       req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
     }
-  
+
     return req;
   });
 export const fetchPost = (id) => API.get(`/posts/${id}`);
